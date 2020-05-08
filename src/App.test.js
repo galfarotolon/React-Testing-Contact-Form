8c72f7f98d820db540c1ad4e1412f60782478b84
Guillermo Alfaro <galfarotolon@gmail.com>
2020-05-07 19:33:02 -0500

    finished mvp

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,56 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+
+test('form can be filled and submitted', () => {
+
+  const { getByTestId } = render(<ContactForm />);
+
+
+  const firstNameInput = getByTestId(/first-name/i);
+  const lastNameInput = getByTestId(/last-name/i);
+  const emailInput = getByTestId(/email/i);
+  const messageInput = getByTestId(/message/i);
+
+  const submitButton = getByTestId(/submit-button/i);
+
+  expect(firstNameInput).toBeVisible();
+  expect(lastNameInput).toBeVisible();
+  expect(emailInput).toBeVisible();
+  expect(messageInput).toBeVisible();
+  expect(submitButton).toBeVisible();
+
+
+})
+
+test('change input values', () => {
+
+
+  const { getByTestId } = render(<ContactForm />);
+
+  const firstNameInput = getByTestId(/first-name/i);
+  const lastNameInput = getByTestId(/last-name/i);
+  const emailInput = getByTestId(/email/i);
+  const messageInput = getByTestId(/message/i);
+
+  const submitButton = getByTestId(/submit-button/i);
+  fireEvent.change(firstNameInput, { target: { value: 'Guillermo' } })
+  fireEvent.change(lastNameInput, { target: { value: 'Alfaro' } })
+  fireEvent.change(emailInput, { target: { value: 'galfarotolon@gmail.com' } })
+  fireEvent.change(messageInput, { target: { value: 'generic message example' } })
+
+  // fireEvent.click(submitButton);
+
+  expect(firstNameInput).toBeInTheDocument();
+  expect(lastNameInput).toBeInTheDocument();
+  expect(emailInput).toBeInTheDocument();
+  expect(messageInput).toBeInTheDocument();
+
+
+})
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 
 const ContactForm = () => {
   const [data, setData] = useState();
+
   const { register, errors, handleSubmit } = useForm({
     mode: "onBlur",
   });
@@ -10,15 +11,23 @@ const ContactForm = () => {
     setData(data);
   };
 
+
+
+
+
   return (
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            id='firstName'
             name="firstName"
+            type='text'
             placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            data-testid='first-name'
+
+            ref={register({ required: true })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -28,8 +37,11 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id='lastName'
             name="lastName"
+            type='text'
             placeholder="Burke"
+            data-testid='last-name'
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -41,21 +53,21 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" id='email' type='email' data-testid='email' ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea name="message" id='message' data-testid='message' ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid='submit-button' type="submit" />
       </form>
     </div>
   );
